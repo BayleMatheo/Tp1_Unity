4cb68e67b68887200100ab558caceb632cfc4c8e
bmatheo <matheo.bayle@ynov.com>
2024-01-09 11:14:18 +0100

    refont

--- Assets/Script/playerMove.cs
+++ Assets/Script/playerMove.cs
@@ -2,26 +2,47 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    private Rigidbody2D _rb;
+    private bool _move;
+    private float _turn;
+    public float _speed = 1.0f;
+    public float _turnSpeed = 1.0f;
+    // private Vector2 movedirection;
 
-    private float speed = 4f;
-    private Vector2 movedirection;
-
-    private void Start()
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
 
     private void Update()
     {
-        movedirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _move = Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow)) {
+            _turn = 1.0f;
+        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            _turn = -1.0f;
+        }else{
+            _turn = 0.0f;
+        }
+        // movedirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(movedirection.x,movedirection.y, 0f) * speed;
+        if (_move)
+        {
+            _rb.AddForce(this.transform.up * this._speed);
+        }
+
+        if (_turn != 0.0f)
+        {
+            _rb.AddTorque(_turn * this._turnSpeed );
+        }
+        
+        // _rb.velocity = new Vector3(movedirection.x,movedirection.y, 0f) * speed;
     }
 }
 
